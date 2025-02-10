ff87ada83583ab138f892f3b532c6dcdd6fdd9a8
Anikait Singh <singh.anikait@gmail.com>
2025-02-10 13:01:49 -0800

    updated relabeling

--- pretraining_analysis/relabel_olmo_pretrain.py
+++ pretraining_analysis/relabel_olmo_pretrain.py
@@ -4,16 +4,20 @@ import openai
 from absl import app, flags
 import tenacity
 
-flags.DEFINE_integer('shard', 0, 'Shard to process')
-flags.DEFINE_integer('num_shards', 1, 'Number of shards to process')
+flags.DEFINE_integer('start', -1, 'Shard to process')
+flags.DEFINE_integer('end', -1, 'Number of shards to process')
 flags.DEFINE_string('split', 'train', 'Split to process')
-flags.DEFINE_integer('max_examples', 10000, 'Max examples to process')
+flags.DEFINE_integer('max_examples', 1000000, 'Max examples to process')
+flags.DEFINE_integer('save_every', 1000, 'Save every N examples')
+flags.DEFINE_string('user', 'Asap7772', 'User to push the dataset to')
 FLAGS = flags.FLAGS
 
 PROMPT_LOC_DICT = {
     'backtracking': '/home/anikait.singh/TinyZero/pretraining_analysis/prompts/backtracking_v0.txt',
     'is_solution': '/home/anikait.singh/TinyZero/pretraining_analysis/prompts/is_solution_v0.txt',
     'verification': '/home/anikait.singh/TinyZero/pretraining_analysis/prompts/verification_v0.txt',
+    'subgoal_setting': '/home/anikait.singh/TinyZero/pretraining_analysis/prompts/subgoal_setting_v0.txt',
+    'backward_chaining': '/home/anikait.singh/TinyZero/pretraining_analysis/prompts/backward_chaining_v0.txt',
 }
 PROMPTS = {
     k: open(v).read() for k, v in PROMPT_LOC_DICT.items()
@@ -34,6 +38,16 @@ def get_response(prompt_type, response, model="meta-llama/Meta-Llama-3.3-70B-Ins
     )
     return output.choices[0].message.content
 
+def map_fn(examples):
+    examples['backtracking_raw'] = [''] * len(examples['text'])
+    examples['is_solution_raw'] = [''] * len(examples['text'])
+    examples['verification_raw'] = [''] * len(examples['text'])
+    for i, response in enumerate(examples['text']):
+        examples['backtracking_raw'][i] = get_response('backtracking', response)
+        examples['is_solution_raw'][i] = get_response('is_solution', response)
+        examples['verification_raw'][i] = get_response('verification', response)
+    return examples
+
 def main(_):
     global client
     client = openai.Client(base_url="http://127.0.0.1:30000/v1", api_key="None")
@@ -42,26 +56,32 @@ def main(_):
     if FLAGS.max_examples > 0:
         ds = ds.select(range(FLAGS.max_examples))
     
-    if FLAGS.num_shards > 1:
-        print(f'Sharding dataset into {FLAGS.num_shards} shards, processing shard {FLAGS.shard}')
-        ds = ds.shard(num_shards=FLAGS.num_shards, index=FLAGS.shard)
+    if FLAGS.start >= 0 and FLAGS.end >= 0 and FLAGS.start < FLAGS.end:
+        print('Subsampling the dataset with start={} and end={}'.format(FLAGS.start, FLAGS.end))
+        ds = ds.select(range(FLAGS.start, FLAGS.end))
 
-    def map_fn(examples):
-        examples['backtracking_raw'] = [''] * len(examples['text'])
-        examples['is_solution_raw'] = [''] * len(examples['text'])
-        examples['verification_raw'] = [''] * len(examples['text'])
-        for i, response in enumerate(examples['text']):
-            examples['backtracking_raw'][i] = get_response('backtracking', response)
-            examples['is_solution_raw'][i] = get_response('is_solution', response)
-            examples['verification_raw'][i] = get_response('verification', response)
-        return examples
-
-    ds = ds.map(map_fn, batched=True, num_proc=os.cpu_count())
+    num_shards = len(ds) // FLAGS.save_every
+    all_ds = []
+    for shard_idx in range(num_shards):
+        curr_shard = ds.select(range(shard_idx * FLAGS.save_every, (shard_idx + 1) * FLAGS.save_every))
+        mapped_shard = curr_shard.map(map_fn, batched=True, num_proc=os.cpu_count())
+        all_ds.append(mapped_shard)
+        
+        # Save the dataset
+        ds_so_far = datasets.concatenate_datasets(all_ds)
+        if FLAGS.start >= 0 and FLAGS.end >= 0 and FLAGS.start < FLAGS.end:
+            suffix = f'_{FLAGS.start}_{FLAGS.end}'
+        else:
+            suffix = ''
+        ds_out_name = f'{FLAGS.user}open_web_math_raw{suffix}'
+        ds_so_far.push_to_hub(ds_out_name)
     
-    if FLAGS.num_shards > 1:
-        ds.push_to_hub(f'Asap7772/open-web-math-processed-{FLAGS.shard+1}-of-{FLAGS.num_shards}')
+    if FLAGS.start >= 0 and FLAGS.end >= 0 and FLAGS.start < FLAGS.end:
+        suffix = f'_{FLAGS.start}_{FLAGS.end}'
     else:
-        ds.push_to_hub('Asap7772/open-web-math-processed')
+        suffix = ''
+    ds_out_name = f'{FLAGS.user}open_web_math_raw{suffix}'
+    ds.push_to_hub(ds_out_name)
     
 if __name__ == '__main__':
     app.run(main)
