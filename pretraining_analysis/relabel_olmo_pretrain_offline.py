09e7e627c44b3c81bc090d808457e99af446e295
Kanishk Gandhi <kanishk050296@gmail.com>
2025-02-10 18:38:19 -0800

    debug

--- pretraining_analysis/relabel_olmo_pretrain_offline.py
+++ pretraining_analysis/relabel_olmo_pretrain_offline.py
@@ -30,7 +30,6 @@ def get_prompts(ds, tokenizer, prompt_templates):
     samples = []
     max_seq_length = 4096
     for e, example in tqdm(enumerate(tokenized_inputs['input_ids']), desc="Truncating prompts"):
-        import pdb; pdb.set_trace()
         if len(example) > max_seq_length-1024:
             sample = tokenizer.decode(example[: max_seq_length - 1024])
             sample = sample[: sample.rfind("\n")]
