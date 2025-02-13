a457c680cbadc8a2dc5fe7fdda86c8798f99e961
Kanishk Gandhi <kanishk050296@gmail.com>
2025-02-13 00:15:29 -0800

    debug

--- pretraining_analysis/relabel_olmo_pretrain_qa.py
+++ pretraining_analysis/relabel_olmo_pretrain_qa.py
@@ -130,7 +130,7 @@ Here is the text:
                 suffix = f'_{args.start}_{args.end}'
             else:
                 suffix = ''
-            ds_out_name = f'{args.user}open_web_math_raw{suffix}'
+            ds_out_name = f'{args.user}open_web_math_raw_qa{suffix}'
             ds_so_far.push_to_hub(ds_out_name)
         except Exception as e:
             print(f'Error saving dataset: {e}')
