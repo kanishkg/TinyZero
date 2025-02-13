32d3764132cfc090527487ea94303c9d8f43ae48
Kanishk Gandhi <kanishk050296@gmail.com>
2025-02-13 12:57:04 -0800

    debug

--- pretraining_analysis/generate_qa_parquets.py
+++ pretraining_analysis/generate_qa_parquets.py
@@ -1,7 +1,6 @@
 import datasets
 
-def filter_fn(example):
-    
+
 
 data_names = [
     'obiwan96/obiwan96open_web_math_qa_0_11616', 
@@ -16,6 +15,13 @@ for data_name in data_names:
     all_ds.append(ds)
 
 ds = datasets.concatenate_datasets(all_ds)
+
+# filter out empty completions and queries
+ds = ds.filter(lambda x: len(x['query']) > 0 and len(x['completion']) > 0)
+
+prefix = """A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant first thinks about the reasoning process in the mind and then provides the user with the answer.
+User: {query} Show your work in <think> </think> tags. And return the final answer in <answer> </answer> tags.
+Assistant: Let me solve this step by step."""
 
 ds_out_name = 'obiwan96/obiwan96open_web_math_qa'
 ds.push_to_hub(ds_out_name)
