e0a3a98108a1dc20fa54587b72d1e8468035b75c
Kanishk Gandhi <kanishk050296@gmail.com>
2025-02-14 16:29:49 -0800

    debug

--- pretraining_analysis/relabel_olmo_pretrain_qa.py
+++ pretraining_analysis/relabel_olmo_pretrain_qa.py
@@ -96,6 +96,8 @@ So, the thoughts should be in the first person, and should look like the author 
 Include the mistakes made by the author in the thoughts section. If the author makes a mistake, include the mistake in the thoughts section.
 Use present tense in the thoughts section. The thoughts section should look like the author is thinking out loud.
 This will come with the realization from the author that they made a mistake.
+DO not add new information, only use what is in the text. Don't add backtracking, verification, or subgoal setting if it is not in the text.
+
 Now do it for this text:""",
 }
 
