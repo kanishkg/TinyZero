53dd49e757b2061945aa447efdfc3570d35de6ea
Kanishk Gandhi <kanishk050296@gmail.com>
2025-02-19 12:24:45 -0800

    debug

--- examples/data_preprocess/math_dataset.py
+++ examples/data_preprocess/math_dataset.py
@@ -31,7 +31,7 @@ def make_prefix(question, template_type):
     # NOTE: also need to change reward_score/countdown.py
     if template_type == 'base':
         """This works for any base model"""
-        prefix = f"""A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant first thinks about the reasoning process in the mind and then provides the user with the answer.
+        prefix = f"""A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant first thinks about the reasoning process in the mind and then provides the user with the answer. The assistant outputs the final answer within \\boxed{{}} tags.
 User: {question}
 Assistant: Let me solve this step by step.
 <think>"""
