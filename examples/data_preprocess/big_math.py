fa307aab20b7c6150a1d295c6320676899abcb6c
Kanishk Gandhi <kanishk050296@gmail.com>
2025-02-24 16:10:12 -0800

    debug

---
+++ examples/data_preprocess/big_math.py
@@ -0,0 +1,108 @@
+# Copyright 2024 Bytedance Ltd. and/or its affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Preprocess the BIGMATH dataset to parquet format
+"""
+
+import os
+import datasets
+
+from verl.utils.hdfs_io import copy, makedirs
+import argparse
+
+# from verl.utils.reward_score.math import remove_boxed, last_boxed_only_string
+from verl.utils.reward_score.math_eval import MathEvaluator
+
+
+def extract_solution(solution_str):
+    # return remove_boxed(last_boxed_only_string(solution_str))
+    return MathEvaluator.get_answer_expr(solution_str)
+
+def make_prefix(question, template_type):
+    # NOTE: also need to change reward_score/countdown.py
+    if template_type == 'base':
+        """This works for any base model"""
+        prefix = f"""A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant first thinks about the reasoning process in the mind and then provides the user with the answer.
+User: {question} Show your work in <think> </think> tags. And return the final answer in <answer> </answer> tags, for example <answer> 5 </answer>.
+Assistant: Let me solve this step by step. 
+<think>"""
+    elif template_type == 'base-2':
+        prefix = f"""{question}
+Show your work in <think> </think> tags. And return the final answer in <answer> </answer> tags. For example, <answer> (1 + 2) / 3 </answer>.
+Let me see if I can solve this step by step.
+<think>"""
+    elif template_type == 'qwen-instruct':
+        """This works for Qwen Instruct Models"""
+        prefix = f"""<|im_start|>system\nYou are a helpful assistant. You first thinks about the reasoning process in the mind and then provides the user with the answer.<|im_end|>\n<|im_start|>user\n {question} Show your work in <think> </think> tags. And return the final answer in <answer> </answer> tags, for example <answer> (1 + 2) / 3 </answer>.<|im_end|>\n<|im_start|>assistant\nLet me solve this step by step.\n<think>"""
+    return prefix
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--local_dir', default='~/ba/data/big_math')
+    parser.add_argument('--hdfs_dir', default=None)
+    parser.add_argument('--template_type', type=str, default='base')
+
+    args = parser.parse_args()
+
+    data_source = 'RLAIF/math'
+    dataset = datasets.load_dataset(data_source, trust_remote_code=True)
+
+    data_train_rl = datasets.load_dataset('RLAIF/Big-Math-RL-Verified', trust_remote_code=True)
+
+    train_dataset = data_train_rl['train']
+    test_dataset = dataset['test']
+
+    # instruction_following = "Let's think step by step and output the final answer within \\boxed{}."
+
+    # add a row to each data item that represents a unique id
+    def make_map_fn(split):
+        def process_fn(example, idx):
+            question = example.pop('problem')
+
+            # question = question + ' ' + instruction_following
+            question = make_prefix(question, template_type=args.template_type)
+
+            answer = example.pop('answer')
+            data = {
+                "data_source": data_source,
+                "prompt": [{
+                    "role": "user",
+                    "content": question
+                }],
+                "ability": "math",
+                "reward_model": {
+                    "style": "rule",
+                    "ground_truth": answer
+                },
+                "extra_info": {
+                    'split': split,
+                    'index': idx
+                }
+            }
+            return data
+        return process_fn
+
+    train_dataset = train_dataset.map(function=make_map_fn('train'), with_indices=True)
+    test_dataset = test_dataset.map(function=make_map_fn('test'), with_indices=True)
+
+    local_dir = args.local_dir
+    hdfs_dir = args.hdfs_dir
+
+    train_dataset.to_parquet(os.path.join(local_dir, 'train.parquet'))
+    test_dataset.to_parquet(os.path.join(local_dir, 'test.parquet'))
+
+    if hdfs_dir is not None:
+        makedirs(hdfs_dir)
+        copy(src=local_dir, dst=hdfs_dir)
